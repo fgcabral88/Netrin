20d30158f33b15c6bffc00e2cd9b739af5c90814
Felipe <fgcabral@yahoo.com>
2024-12-12 12:07:00 -0300

    #feat: Parte 1

---
+++ Netrin/Presentation/Filters/AcaoFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog;
+
+namespace Netrin.Api.Presentation.Filters
+{
+    /// <summary>
+    /// Filtros de Ação - Permitem executar lógica antes ou depois de uma ação do controlador
+    /// </summary>
+    public class AcaoFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Log.Information("Executando ação: {ActionName}", context.ActionDescriptor.DisplayName);
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Log.Information("Ação concluída: {ActionName}", context.ActionDescriptor.DisplayName);
+        }
+    }
+}
